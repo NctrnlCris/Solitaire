87c7e96b5c539a248177c0de5490573557386051
NctrnlCris <99302274+NctrnlCris@users.noreply.github.com>
2024-04-01 16:44:42 -0700

    Add files via upload

---
+++ Solitaire_Project/solitaire.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+enum Suit{
+    DIAMONDS, CLUBS, HEARTS, SPADES
+};
+
+enum Value{
+    valueA, value1, value2, value3, value4, value5, value6, value7, value8, value9, value10, valueJ, valueQ, valueK
+
+};
+
+typedef struct{
+    int value;
+    int suit;
+
+} Card;
+
+void Shuffle()
+
+int main (void)
+{
+
+}
